2db5551742e52ccd04a839f192747205715d2cff
Yu Shigetani <s2g4t1n2@gmail.com>
2020-02-22 14:36:56 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          }
+        ],
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    inline: true,
+    hot: true,
+    port: 3000,
+    publicPath: 'http://localhost:3000/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+    }),
+  ]
+}
