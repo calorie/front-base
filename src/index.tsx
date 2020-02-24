770282bd3d644212a9d09492b9a1ddc83a06cec5
Yu Shigetani <s2g4t1n2@gmail.com>
2020-02-24 21:28:24 +0900

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
 import App from './App'
+import { index } from './modules'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const storeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  combineReducers({
+    state: index,
+  }),
+  storeEnhancers(applyMiddleware(logger))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ src/modules/index.ts
@@ -0,0 +1,8 @@
+const initialState = {}
+
+export const index = (state = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
